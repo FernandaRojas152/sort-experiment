3bac44cb9f62762eb783094e34ebca1abced40b2
Nicolas Colmenares <47890683+NickEsColR@users.noreply.github.com>
2021-03-11 17:37:23 -0500

    interface working

---
+++ sort-experiment/ui/Interface.Designer.cs
@@ -0,0 +1,203 @@
+﻿
+namespace sort_experiment.ui
+{
+    partial class Interface
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSelectionTh = new System.Windows.Forms.TextBox();
+            this.txtQuicksortTh = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSelectionH = new System.Windows.Forms.TextBox();
+            this.txtQuicksortH = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(198, 150);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(122, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Selection Time";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(266, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Quicksort Time";
+            // 
+            // txtSelectionTh
+            // 
+            this.txtSelectionTh.Enabled = false;
+            this.txtSelectionTh.Location = new System.Drawing.Point(116, 112);
+            this.txtSelectionTh.Name = "txtSelectionTh";
+            this.txtSelectionTh.Size = new System.Drawing.Size(100, 20);
+            this.txtSelectionTh.TabIndex = 3;
+            // 
+            // txtQuicksortTh
+            // 
+            this.txtQuicksortTh.Enabled = false;
+            this.txtQuicksortTh.Location = new System.Drawing.Point(257, 112);
+            this.txtQuicksortTh.Name = "txtQuicksortTh";
+            this.txtQuicksortTh.Size = new System.Drawing.Size(100, 20);
+            this.txtQuicksortTh.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Tamaño 100";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Tamaño 1000";
+            // 
+            // txtSelectionH
+            // 
+            this.txtSelectionH.Enabled = false;
+            this.txtSelectionH.Location = new System.Drawing.Point(116, 77);
+            this.txtSelectionH.Name = "txtSelectionH";
+            this.txtSelectionH.Size = new System.Drawing.Size(100, 20);
+            this.txtSelectionH.TabIndex = 7;
+            // 
+            // txtQuicksortH
+            // 
+            this.txtQuicksortH.Enabled = false;
+            this.txtQuicksortH.Location = new System.Drawing.Point(257, 77);
+            this.txtQuicksortH.Name = "txtQuicksortH";
+            this.txtQuicksortH.Size = new System.Drawing.Size(100, 20);
+            this.txtQuicksortH.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(222, 80);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(14, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "S";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(222, 115);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(14, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "S";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(363, 80);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(14, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "S";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(363, 115);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(14, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "S";
+            // 
+            // Interface
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 185);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtQuicksortH);
+            this.Controls.Add(this.txtSelectionH);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtQuicksortTh);
+            this.Controls.Add(this.txtSelectionTh);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Interface";
+            this.Text = "Sort Experiment";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSelectionTh;
+        private System.Windows.Forms.TextBox txtQuicksortTh;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSelectionH;
+        private System.Windows.Forms.TextBox txtQuicksortH;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ sort-experiment/ui/Interface.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sort_experiment.ui
+{
+    public partial class Interface : Form
+    {
+        public Interface()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            txtQuicksortH.Text = "100";
+            txtQuicksortTh.Text = "1000";
+            txtSelectionH.Text = "100";
+            txtSelectionTh.Text = "1000";
+        }
+    }
+}
