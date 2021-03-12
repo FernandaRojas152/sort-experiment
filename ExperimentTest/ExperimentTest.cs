3045835136d4c7559c0a278a6fc49497be504223
Fernanda <FernandaRojas152@hotmail.com>
2021-03-12 11:14:06 -0500

    all selection test added

---
+++ ExperimentTest/ExperimentTest.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using sort_experiment.model;
+
+namespace ExperimentTest
+{
+    [TestClass]
+    public class ExperimentTest
+    {
+        private int[] arr;
+        private Random random = new Random();
+
+
+        [TestMethod]
+        public void SetUpScenary()
+        {
+            arr = new int[10];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next();
+            }
+           Experiment.SetArr(arr);
+        }
+
+        [TestMethod]
+        public void SetUpScenary1()
+        {
+            arr = new int[100];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next();
+            }
+            Experiment.SetArr(arr);
+        }
+
+        [TestMethod]
+        public void SetUpScenary2()
+        {
+            arr = new int[1000];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next();
+            }
+            Experiment.SetArr(arr);
+        }
+
+        [TestMethod]
+        public void SelectionSort()
+        {
+            for (int j = 0; j < 1000; j++)
+            {
+                SetUpScenary();
+                Experiment.Selection();
+            }
+        }
+
+        [TestMethod]
+        public void SelectionSort1()
+        {
+            for (int j = 0; j < 1000; j++)
+            {
+                SetUpScenary1();
+                Experiment.Selection();
+            }
+        }
+
+        [TestMethod]
+        public void SelectionSort2()
+        {
+            for (int j = 0; j < 1000; j++)
+            {
+                SetUpScenary2();
+                Experiment.Selection();
+            }
+        }
+    }
+}
--- sort-experiment/model/Experiment.cs
+++ sort-experiment/model/Experiment.cs
@@ -6,9 +6,39 @@ using System.Threading.Tasks;
 
 namespace sort_experiment.model
 {
-    class Experiment
+    public static class Experiment
     {
-        private int[] array;
+		private static int[] arr;
 
-    }
+		public static int[] GetArr()
+		{
+			return arr;
+		}
+
+		public static void SetArr(int[] value)
+		{
+			arr = value;
+		}
+
+		public static void Selection()
+		{
+			int n = arr.Length;
+
+			for (int i = 0; i < n - 1; i++)
+			{
+				int min_idx = i;
+				for (int j = i + 1; j < n; j++)
+				{
+					if (GetArr()[j] < GetArr()[min_idx])
+					{
+						min_idx = j;
+					}
+				}
+
+				int temp = GetArr()[min_idx];
+				GetArr()[min_idx] = GetArr()[i];
+				GetArr()[i] = temp;
+			}
+		}
+	}
 }
